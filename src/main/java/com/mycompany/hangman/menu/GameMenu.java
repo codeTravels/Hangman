0d060ea404644acecd59d968c229ade6fd497eb0
Cory Bianchi <coryjesus@hotmail.com>
2017-10-28 18:52:56 +0200

    used different API to create separator

--- src/main/java/com/mycompany/hangman/menu/GameMenu.java
+++ src/main/java/com/mycompany/hangman/menu/GameMenu.java
@@ -10,7 +10,6 @@ import com.mycompany.hangman.actions.NewGameAction;
 import javax.swing.Action;
 import javax.swing.JMenu;
 import javax.swing.JMenuItem;
-import javax.swing.JSeparator;
 import com.mycompany.hangman.actions.ActionRepository;
 
 /**
@@ -31,7 +30,7 @@ public class GameMenu extends JMenu
         JMenuItem jMenuItem = new JMenuItem(action);
         add(jMenuItem);
 
-        add(new JSeparator());
+        addSeparator();
 
         action = actionRepo.get(ExitAction.class);
         jMenuItem = new JMenuItem(action);
