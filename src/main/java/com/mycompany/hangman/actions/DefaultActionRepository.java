e305f30d01d839bf044f2621cd0586c0a0f01d63
Cory Bianchi <coryjesus@hotmail.com>
2017-11-06 18:27:34 +0100

    renamed AbstractRepository to InMemoryRepository

--- src/main/java/com/mycompany/hangman/actions/DefaultActionRepository.java
+++ src/main/java/com/mycompany/hangman/actions/DefaultActionRepository.java
@@ -1,18 +1,13 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.mycompany.hangman.actions;
 
-import com.mycompany.hangman.repo.AbstractRepository;
+import com.mycompany.hangman.repo.InMemoryRepository;
 import javax.swing.Action;
 
 /**
  *
  * @author Cory
  */
-public class DefaultActionRepository extends AbstractRepository<Action> implements ActionRepository
+public class DefaultActionRepository extends InMemoryRepository<Action> implements ActionRepository
 {
 
 }
--- src/main/java/com/mycompany/hangman/repo/InMemoryRepository.java
+++ src/main/java/com/mycompany/hangman/repo/InMemoryRepository.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.mycompany.hangman.repo;
 
 import java.util.HashMap;
@@ -13,11 +8,12 @@ import java.util.Map;
  * @author Cory
  * @param <T>
  */
-public abstract class AbstractRepository<T> implements Repository<T>
+public class InMemoryRepository<T> implements Repository<T>
 {
+
     private final Map<String, T> objectMap = new HashMap<>();
 
-    public AbstractRepository()
+    public InMemoryRepository()
     {
 
     }
